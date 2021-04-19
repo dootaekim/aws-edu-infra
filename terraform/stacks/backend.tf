27ee4e1c590598608321e0313fa7f68643a519b4
dootaekim <dootae.kim@gmail.com>
2021-04-20 02:13:47 +0900

    initial commit

--- terraform/stacks/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-state-file-489178515218"
-    key    = "aws-edu-infra/ap-northeast-2/prd/aws-infra.tfstate"
-    region = "ap-northeast-2"
-  }
-}
