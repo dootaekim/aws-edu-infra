d2166f346b645929e96dce51f8bb832d5d79d575
김두태 선임/클라우드데브옵스팀 <dootae.kim@lgcns.com>
2021-05-10 19:12:40 +0900

    EKS Added

---
+++ terraform/stacks/main.tf
@@ -0,0 +1,94 @@
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluseter_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_availability_zones" "available"{
+
+}
+
+resource "aws_security_group" "worker_group_mgmt_one" {
+  name_prefix = "worker_group_mgmt_one"
+  vpc_id = module.vpc.vpc_id
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+
+    cidr_blocks = [
+      "10.0.0.0/8",
+    ]
+  }
+}
+
+resource "aws_security_group" "all_worker_mgmt" {
+  name_prefix = "all_worker_management"
+  vpc_id = module.vpc.vpc_id
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+
+    cidr_blocks = [
+      "10.0.0.0/8",
+      "172.16.0.0/12",
+      "192.168.0.0/16",
+    ]
+  }
+}
+
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+#   version = "2.78.0"
+  name               = "aws-edu-vpc"
+  cidr               = "10.1.1.0/25"
+  azs                = var.vpc_azs
+  private_subnets    = ["10.1.1.0/27", "10.1.1.32/27"]
+  public_subnets     = ["10.1.1.64/27", "10.1.1.96/27"]
+  enable_nat_gateway = true
+  # 외부에서 들어오는 요청은 api-gateway에서 받으며, 인터넷향 요청이 서비스에 영향 줄 일은 없어 단일 nat 사용해도 서비스에 영향 없을 것으로 보임
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"  = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"  = "1"
+  }
+}
+
+module "eks" {
+  source = "terraform-aws-modules/eks/aws"
+  version = "12.2.0"
+  cluster_name =  var.cluster_name
+  cluster_version = "1.18"
+  subnets = module.vpc.private_subnets
+  cluster_create_timeout = "1h"
+  cluster_endpoint_private_access = true
+  vpc_id = module.vpc.vpc_id
+
+  worker_groups = [
+    {
+      name = "worker-group-1"
+      instance_type = "t2.small"
+      additional_userdata = "echo foo bar"
+      asg_desired_capaciry = 1
+      additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
+    }
+  ]
+
+  worker_additional_security_group_ids = [aws_security_group.all_worker_mgmt.id]
+  map_roles = var.map_roles
+  map_users = var.map_users
+  map_accounts = var.map_accounts
+}
--- terraform/stacks/variables.tf
+++ terraform/stacks/variables.tf
@@ -4,6 +4,7 @@ variable "environment" {
 }
 variable "aws_region" {
   type = string
+  default = "ap-southeast-2"
 }
 
 ########## Terraform State File Configs ########## 
@@ -28,4 +29,40 @@ variable "s3_tfstate_file" {
 ########## VPC configuration ##########
 variable "vpc_azs" {
   type = list
+}
+
+########## EKS configuration ##########
+variable "cluster_name" {
+  default = "getting-started-eks"
+}
+
+variable "map_accounts" {
+  description = "Additional AWS account numbers to add to the aws-auth configmap"
+  type = list(string)
+
+  default = [
+    "77778777777",
+    "88989898988",
+  ]
+}
+variable "map_roles" {
+  description = "Additional IAM roles to add to the aws-auth configmap"
+  type = list(object({
+    rolearn = string
+    username = string
+    groups = list(string)
+  }))
+
+  default = [
+    {
+      rolearn = "arn:aws:iam::777777777777:role/role1"
+      username = "role1"
+      groups = ["system:masters"]
+    },
+    {
+      rolearn = "arn:aws:iam::777777777777:role/role2"
+      username = "role2"
+      groups = ["system:masters"]
+    },
+  ]
 }
